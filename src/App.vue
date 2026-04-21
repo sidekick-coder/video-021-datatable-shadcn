a04edfc2761896439daecf74e1253590d3e5ccf3
Henrique <henrique@sidekick-coder.com>
2026-04-21 08:15:01 -0300

    feat: install shadcn and tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
-  <HelloWorld />
+  <div>
+    <Button>Click me</Button>
+  </div>
 </template>
