8c4fe1d76df568171908935382adb3f4ac723cab
Henrique <henrique@sidekick-coder.com>
2026-04-21 08:24:31 -0300

    feat: breakpoint 2

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,3 +1,39 @@
+<script setup lang="ts">
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+</script>
+
 <template>
-    <div>Table</div>
+  <Table>
+    <TableHeader>
+      <TableRow>
+        <TableHead class="w-[100px]">
+          Invoice
+        </TableHead>
+        <TableHead>Status</TableHead>
+        <TableHead>Method</TableHead>
+        <TableHead class="text-right">
+          Amount
+        </TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow>
+        <TableCell class="font-medium">
+          INV001
+        </TableCell>
+        <TableCell>Paid</TableCell>
+        <TableCell>Credit Card</TableCell>
+        <TableCell class="text-right">
+          $250.00
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
 </template>
